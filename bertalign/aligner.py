d5fffc346efb801c55f3f8f8390fc546339f2a8f
Ali Hejazi <seyed.ali.hejazi.35@gmail.com>
2024-01-28 23:04:04 +0330

    Update aligner.py

--- bertalign/aligner.py
+++ bertalign/aligner.py
@@ -8,6 +8,8 @@ class Bertalign:
     def __init__(self,
                  src,
                  tgt,
+                 src_lang,
+                 tgt_lang,
                  max_align=5,
                  top_k=3,
                  win=5,
@@ -26,8 +28,6 @@ class Bertalign:
         
         src = clean_text(src)
         tgt = clean_text(tgt)
-        src_lang = detect_lang(src)
-        tgt_lang = detect_lang(tgt)
         
         if is_split:
             src_sents = src.splitlines()
